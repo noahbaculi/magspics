c37c44c64672ed9d788691055622e284d1088407
Noah <nbaculi@hotmail.com>
2022-06-24 17:33:40 -0700

    add responsive cover img

--- images/image_generator.py
+++ images/image_generator.py
@@ -71,8 +71,8 @@ def convert_folder(base_path: str, widths: list, exclude: list = None, include: 
 
 
 if __name__ == "__main__":
-    ## PORTFOLIO
-    # convert_folder(r".\images\portfolio\salesforce", [400, 800], exclude=["team_lunch_orig.png"])
-    convert_folder(r"C:\Users\Noah\Documents\magspictures\images\portfolio", [400, 800, 1500])
+    # convert_folder(r"C:\Users\Noah\Documents\magspictures\images\portfolio", [400, 800, 1500])
+    # convert_folder(r"C:\Users\Noah\Documents\magspictures\images\icons", [100], include=["instagram", "twitter"])
+    convert_folder(r"C:\Users\Noah\Documents\magspictures\images\cover", [600, 1000, 2000])
 
     print("Done.")
